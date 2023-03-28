a3bd6fe6f54c988a68ff7c9d52d051c3412f5ca1
ArielVinis <arielcabelo15@hotmail.com>
2023-03-28 17:55:28 -0300

    Foi?

--- ProjetoTeste/src/ClassTeste.java
+++ ProjetoTeste/src/ClassTeste.java
@@ -1,6 +1,14 @@
 public class ClassTeste {
     public static void main(String[] args) {
         
+        String nome = "Ariel";
+        int idade = 25;
+        double peso = 65.5;
+        
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Minha idade é: " + idade);
+        System.out.println("Eu peso: " + peso);
+        
         System.out.println("Hello, World!");
 
 
