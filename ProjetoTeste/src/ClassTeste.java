5568dcf5a04d7d323d30e5ada21a0d8ae9c6970b
ArielVinis <arielcabelo15@hotmail.com>
2023-04-17 13:09:32 -0300

    Teste do Teste

--- ProjetoTeste/src/ClassTeste.java
+++ ProjetoTeste/src/ClassTeste.java
@@ -10,6 +10,7 @@ public class ClassTeste {
             System.out.println("Eu peso: " + peso); 
         
             System.out.println("Teste realizado");
-        
+            System.out.println("Hello World")
+
     }
 }
