21aae48b66b32fdc57bc26a9fc32343f97651777
ArielVinis <arielcabelo15@hotmail.com>
2023-04-17 14:41:37 -0300

    Operador Unario

--- ProjetoTeste/src/OperadorUnario.java
+++ ProjetoTeste/src/OperadorUnario.java
@@ -2,9 +2,13 @@ public class OperadorUnario {
     public static void main(String[] args) {
         int numero = 5;
 
-        System.out.println(- numero);
-
-        System.out.println(numero);
+            System.out.println(numero);
+            System.out.println(numero --);
         
+        boolean variavel = true;
+
+            variavel = !variavel;
+
+            System.out.println(variavel);
     }
 }
