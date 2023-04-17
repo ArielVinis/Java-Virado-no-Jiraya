754608c515faf3e26a29a0178191a8221d8e440f
ArielVinis <arielcabelo15@hotmail.com>
2023-04-17 13:00:11 -0300

    Operador Unario

--- ProjetoTeste/src/ClasseOperadores.java
+++ ProjetoTeste/src/ClasseOperadores.java
@@ -1,11 +1,25 @@
+import java.util.Date;
+
 public class ClasseOperadores {
     public static void main(String[] Args) {
         String nome = "Ariel";
-        int idade = 22;
+        int idade = 27;
         double peso = 68.5;
         char sexo = 'M';
         boolean doadorOrgao = false;
-        Date dataNascimento = new Date();
+        Date dataNascimento = new Date(1997/11/27);
+
+        long millis=System.currentTimeMillis();  
+        java.sql.Date date=new java.sql.Date(millis);  
+
+
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Tenho " + idade + " anos de idade.");
+        System.out.println("No momento estou pesando: " + peso);
+        System.out.println("Sou do sexo: " + sexo);
+        System.out.println("Doador de orgão? " + doadorOrgao);
+        System.out.println("Minha data de nascimento é: " + dataNascimento);
+        System.out.println(date);
 
     }
 }
