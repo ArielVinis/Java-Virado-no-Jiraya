39086d2b1dc9a5aa7e1d660938093658ccbf334e
ArielVinis <arielcabelo15@hotmail.com>
2023-05-08 12:11:41 -0300

    Nomenclatura correta

---
+++ ProjetoAulas/src/Orientacao_a_objetos/Cadeira.java
@@ -0,0 +1,17 @@
+package Orientacao_a_objetos;
+
+public class Cadeira {
+    float altura;
+    float altura_assento;
+    String cor = "Marrom";
+
+        void serrar (){
+            System.out.println("Serrando a madeira...");
+    }
+        void lixar(){
+            System.out.println("Lixando a madeira...");
+    }
+        void pintar (){
+            System.out.println("Pintando a madeira...");
+    }
+}
---
+++ ProjetoAulas/src/Orientacao_a_objetos/inicio.java
@@ -0,0 +1,16 @@
+package Orientacao_a_objetos;
+
+public class inicio {
+    public static void main(String[] args) {
+        Cadeira c1 = new Cadeira();
+        c1.serrar();
+        c1.lixar();
+        c1.pintar();
+    System.out.println("Cor atual da cadeira é: " + c1.cor);
+
+       /*  Cadeira c2 = new Cadeira();
+        c2.serrar();
+        c2.lixar();
+        c2.pintar(); */
+    }
+}
