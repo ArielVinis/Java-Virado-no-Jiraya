bc2821201bca744b5d7ba7b2f022cb04e54f7bbf
ArielVinis <arielcabelo15@hotmail.com>
2023-05-11 17:57:31 -0300

    Try-Catch-Finally

--- ProjetoAulas/src/Comentários_Erros_e_TryCatch/TratamentoDeExceção.java
+++ ProjetoAulas/src/Comentários_Erros_e_TryCatch/TratamentoDeExceção.java
@@ -4,12 +4,20 @@ public class TratamentoDeExceção {
     public static void main(String[] args) {
         
         try {
-            System.out.println(args[0]);
+            System.out.println(args[0] + "\n\n");
+
+            double valor = Double.parseDouble(args[0]);
+            System.out.println(valor);
+            
         } catch (ArrayIndexOutOfBoundsException e) {
             /* "Ctrl + espaço"
             classe para tratamento de exceção
             Quando estoura o array. */
-        System.out.println("Nenhum argumento informado! " + e.getMessage());
+        System.out.println("Nenhum argumento informado! ");
+        }
+        // O finally, independente do resultado, ele vai mostrar a mensagem final"
+        finally{
+            System.out.println("Obrigado!");
         }
 
     }
