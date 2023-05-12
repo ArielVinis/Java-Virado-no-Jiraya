06ee68c41b2321ebf0dde1c4733b475ed3120798
ArielVinis <arielcabelo15@hotmail.com>
2023-05-12 13:02:27 -0300

    Comentarios, Erros e TryCatch

--- ProjetoAulas/src/Comentários_Erros_e_TryCatch/TratamentoDeExceção.java
+++ ProjetoAulas/src/Comentários_Erros_e_TryCatch/TratamentoDeExceção.java
@@ -7,18 +7,32 @@ public class TratamentoDeExceção {
             System.out.println(args[0] + "\n\n");
 
             double valor = Double.parseDouble(args[0]);
-            System.out.println(valor);
-            
-        } catch (ArrayIndexOutOfBoundsException e) {
-            /* "Ctrl + espaço"
-            classe para tratamento de exceção
-            Quando estoura o array. */
-        System.out.println("Nenhum argumento informado! ");
+                System.out.println(valor);
         }
+
+        catch (Exception e){
+            System.out.println("Só Deus sabe o que aconteceu.\n" + getCause());
+        }
+
+        /* catch (ArrayIndexOutOfBoundsException e) {
+            // "Ctrl + espaço"
+            // classe para tratamento de exceção
+            // Quando estoura o array.
+            System.out.println("Estourou o Array!");
+        }
+
+        catch (NumberFormatException e) {
+            System.out.println("Valor inválido");
+        } */
+        
         // O finally, independente do resultado, ele vai mostrar a mensagem final"
         finally{
             System.out.println("Obrigado!");
         }
+        
+    }
 
+    private static String getCause() {
+        return ("Descubra sozinho. Fé!");
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Sobre as aulas
+# Sobre as aulas JAVA.
 ## Como estou aprendendo, será bom deixar registrado as matérias já commitadas para voltar aqui, caso tenha alguma dúvida ou esqueça algo.
 
-Neste momento estou estudando sobre Programação Orientada a Objetos.
+### Já foi estudado Aqui:
+Metodos
+Sobre Classes
+Classe de Operadores
+Condição Ternaria
+Operadores Lógicos
+Operadores Relacionais
+Operador Unario
 
-![Alt text](../../../Downloads/TryCath.png)
+Orientação a Objetos
+Lista
+Arrays
+Comentários, Erros e TryCatch
