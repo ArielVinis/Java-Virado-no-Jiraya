b0b9b9e8f4d50237e1312a1e56416db0dcc200dd
Exercitando POO - Livro <ariel.franca@hotmail.com>
2024-06-09 20:10:44 -0300

    Operador ternário

---
+++ ProjetoAulas/src/Estruturas/Condicionais02_Ternario.java
@@ -0,0 +1,18 @@
+package Estruturas;
+
+public class Condicionais02_Ternario {
+    public static void main(String[] args) {
+
+        double salario = 3000;
+        String mensagemDoar = "Eu vou doar 500 pro Ariel";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+
+        // (condicao) ? verdadeiro : falso
+        String resultado1 = salario > 2000 ? mensagemDoar : mensagemNaoDoar;
+        // OU
+        String resultado2 = salario > 4000 ? "Eu vou doar 500 pro Ariel" : "Ainda não tenho condições, mas vou ter!";
+
+        System.out.println(resultado1);
+        System.out.println(resultado2);
+    }
+}
