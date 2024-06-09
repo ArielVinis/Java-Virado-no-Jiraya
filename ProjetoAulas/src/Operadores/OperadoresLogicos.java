996f436c2d054f159dfc137adfda2350af9e5565
Exercitando POO - Livro <ariel.franca@hotmail.com>
2024-06-09 12:26:02 -0300

    Aprendendo mais sobre Operadores Logicos

--- ProjetoAulas/src/Operadores/OperadoresLogicos.java
+++ ProjetoAulas/src/Operadores/OperadoresLogicos.java
@@ -7,20 +7,21 @@ public class OperadoresLogicos {
         boolean condicao1 = true;
         boolean condicao2 = true;
 
-        if (condicao1 && condicao2){
+        if (condicao1 && condicao2) {
             System.out.println("As duas condições são verdadeiras.");
         }
 
-        if (condicao1 && (7 > 6)){
+        if (condicao1 && (7 > 6)) {
             System.out.println("As duas condições são verdadeiras.");
         }
 
-        if (condicao1 || condicao2){
+        if (condicao1 || condicao2) {
             System.out.println("Uma das condições é verdadeira.");
         }
 
-            System.out.println("Fim.");
+        System.out.println("Fim.");
 
+        //CRIANDO UMA REGRA EXEMPLIFICADA PARA UTILIZAR || = "OR"
         double valorTotalContaCorrente = 200;
         double valorTotalContaPoupança = 10000;
         float valorPlaystation = 5000F;
@@ -28,6 +29,16 @@ public class OperadoresLogicos {
         boolean isPlaystationCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupança > valorPlaystation;
 
         System.out.println("isPlaystationCompravel " +isPlaystationCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2; // % é RESTO
+
+        System.out.println(bonus);
 
     }
     
