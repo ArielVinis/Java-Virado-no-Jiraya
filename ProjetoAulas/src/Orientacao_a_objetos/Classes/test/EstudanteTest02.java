4c36053581741164c22d655d4a9ec34e5d453856
ArielVinis <ariel.franca@hotmail.com>
2024-09-05 00:19:38 -0300

    EstudanteTest02

---
+++ ProjetoAulas/src/Orientacao_a_objetos/Classes/test/EstudanteTest02.java
@@ -0,0 +1,13 @@
+package Orientacao_a_objetos.Classes.test;
+
+import Orientacao_a_objetos.Classes.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
